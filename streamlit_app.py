50b809ed691c2f15b3f525cbeaebe8f085022527
ameimaris <36538271+ameimaris@users.noreply.github.com>
2025-03-01 21:31:18 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+hide_streamlit_styles = """
+<style>
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_styles, unsafe_allow_html=True)

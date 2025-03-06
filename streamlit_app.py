4df8d8211928e7d64ca926733dc69db3982e3dfa
ameimaris <36538271+ameimaris@users.noreply.github.com>
2025-03-06 14:39:13 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(vv)
 
 title = st.text_input("Input - use a num", "1")
 title  = int(title)
-out = eval(st.secrets["compute"])
+out = eval(f'st.secrets["compute"]')
 st.write("Output", out)
 
 hide_streamlit_styles = """
